f8df814febadaa488c9fdb5ab4e293bcebb07550
Patrick@FullStory <45576380+patrick-fs@users.noreply.github.com>
2019-06-06 12:48:42 -0400

    Update fullstory.js

--- src/api/fullstory.js
+++ src/api/fullstory.js
@@ -1,4 +1,4 @@
-// TODO: host an official FullStory package in npm that defines these functions so they can be imported as a module
+// TODO: host an official FullStory package in npm that defines this module
 
 const FS = () => window[window['_fs_namespace']];
 
